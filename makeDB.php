c17ecc13f1bf23c12e1250855663c8d9cb3181eb
jens <jens@lea.io>
2013-12-08 09:17:38 +0100

    Fixed windows php exec issue

--- makeDB.php
+++ makeDB.php
@@ -1,6 +1,7 @@
-<?
+<?php
 /*==============================================================================
 =            A simple PHP script to dump the contents of a directory           =
 ==============================================================================*/
 $files = []; foreach (glob("content/*") as $filename) $files []= $filename;
 file_put_contents ( "./db.json" , json_encode($files));
+?>
